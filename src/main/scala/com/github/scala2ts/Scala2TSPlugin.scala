56739f68e26659118d64312d6c6c8ad65e7fb9a7
Matt Oliver <mjo2089@gmail.com>
2020-05-15 09:48:18 -0400

    Bump core

--- src/main/scala/com/github/scala2ts/Scala2TSPlugin.scala
+++ src/main/scala/com/github/scala2ts/Scala2TSPlugin.scala
@@ -88,7 +88,7 @@ object Scala2TSPlugin extends AutoPlugin {
   override lazy val projectSettings: Seq[Def.Setting[_]] =
     Seq(
       autoCompilerPlugins := true,
-      addCompilerPlugin("com.github.scala2ts" %% "scala2ts-core" % "1.0.5"),
+      addCompilerPlugin("com.github.scala2ts" %% "scala2ts-core" % "1.0.6"),
       JsEngineKeys.parallelism := 1,
       libraryDependencies ++= Seq(
         "org.webjars.npm" % "typescript" % "3.8.3"
